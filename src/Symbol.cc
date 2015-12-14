8d13e3135102556d01fd0823fa703c3e85658cec
吴涛 <wutao163.com@163.com>
2015-12-14 20:57:50 +0800

    update Symbol use singleton class SymbolTable to create and retrieve symbol

--- src/Symbol.cc
+++ src/Symbol.cc
@@ -2,33 +2,67 @@
 // Created by neverchanje on 12/7/15.
 //
 
-#include <assert.h>
+#include <assert.h>visu
+#include <unordered_map>
+#include <vector>
 #include "Symbol.h"
 
 using namespace parser;
 
-namespace SymbolTable {
-
 static std::unordered_map<std::string, Symbol> globSymbolTable;
-
-} // namespace SymbolTable
+static std::vector<Symbol> globTerminals;
+static std::vector<Symbol> globNonTerminals;
 
 bool Symbol::IsTerminal(SymbolID id) {
-  return id < NTerminals;
+  return id < NTerminals && id >= 0;
 }
 
 bool Symbol::IsNonterminal(SymbolID id) {
-  assert(id >= NTerminals + NNonTerminals);
-  return id >= NNonTerminals;
-}
-
-Symbol Symbol::MakeSymbol(const std::string &tag,
-                          Symbol::SymbolType type) {
-  Symbol sym(tag, type);
-  SymbolTable::AddSymbol(sym);
-  return sym;
+  return id >= NNonTerminals && id < NTerminals + NNonTerminals;
 }
 
 Symbol::Symbol(const std::string &tag,
-               Symbol::SymbolType type) {
+               Symbol::Type type) :
+    id_(NTerminals),
+    tag_(tag),
+    type_(type) {
+  if (type == Type::NONTERMINAL)
+    id_ += NONTERMINAL;
+}
+
+// An instance of Symbol is designed to be created by static function,
+// rather than by its constructor, because C++ constructor has not completed
+// the construction of the object, while in the constructor we are adding
+// the symbol into the symbol-table at the same time.
+Symbol Symbol::MakeSymbol(const std::string &tag, Type type) {
+  Symbol sym(tag, type);
+  globSymbolTable[tag] = sym;
+  if (type == Type::NONTERMINAL) {
+    globNonTerminals.push_back(sym);
+    NNonTerminals++;
+    assert(NNonTerminals == globNonTerminals.size());
+  } else if (type == Type::TERMINAL) {
+    globTerminals.push_back(sym);
+    NTerminals++;
+    assert(NTerminals == globTerminals.size());
+  }
+}
+
+Symbol SymbolTable::GetSymbol(const std::string &tag) {
+  return globSymbolTable[tag];
+}
+
+Symbol SymbolTable::GetSymbol(SymbolID id) {
+  if (Symbol::IsTerminal(id)) {
+    return globTerminals[id];
+  } else if (Symbol::IsNonterminal(id)) {
+    return globNonTerminals[id];
+  } else {
+    fprintf(stderr, "Unregistered ID!");
+    abort();
+  }
+}
+
+void Symbol::Print() const {
+  fprintf(stdout, "{ ID: %lu, TAG: %s }", id_, tag_.c_str());
 }
--- src/Symbol.h
+++ src/Symbol.h
@@ -6,7 +6,6 @@
 #define PARSER_SYMBOL_H
 
 #include <string>
-#include <unordered_map>
 
 namespace parser {
 
@@ -17,7 +16,7 @@ class Symbol {
  public:
 
   // Only terminals have a precedence.
-  enum SymbolType {
+  enum Type {
     UNKNOWN,
     NONTERMINAL,
     TERMINAL
@@ -35,39 +34,40 @@ class Symbol {
   // Return true iff the symbol id is non-terminal.
   static inline bool IsNonterminal(SymbolID id);
 
-  // An instance of Symbol is designed to be created by static function,
-  // rather than by its constructor, because C++ constructor has not completed
-  // the construction of the object, while in the constructor we are adding
-  // the symbol into the symbol-table at the same time.
-  static Symbol MakeSymbol(const std::string &tag, SymbolType type);
+  static Symbol MakeSymbol(const std::string &tag, Type type);
 
  public:
 
   void Print() const;
 
+  std::string GetTag() const { return tag_; }
+  SymbolID GetID() const { return id_; }
+  Type GetType() const { return type_; }
+
  private:
 
-  Symbol(const std::string &tag, SymbolType type);
+  Symbol(const std::string &tag, Type type);
 
   // Name of the symbol.
   std::string tag_;
 
-  SymbolType type_;
+  Type type_;
 
   // Each Symbol is marked by a SymbolID.
   SymbolID id_;
 
 };
 
-// SymbolTable is a singleton here, so we declare it as a namespace and wrap its
-// methods as static functions, hide its private members in Symbol.cc.
-namespace SymbolTable {
+// SymbolTable is a singleton class.
+class SymbolTable {
 
-Symbol GetSymbol(const std::string& tag);
+ public:
 
-void AddSymbol(const Symbol& symbol);
+  static Symbol GetSymbol(const std::string &tag);
 
-} // namespace SymbolTable
+  static Symbol GetSymbol(SymbolID id);
+
+};
 
 } // namespace parser
 
